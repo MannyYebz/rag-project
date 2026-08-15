72acac579a28b4aac32f7225a0a8b49e6e985023
MannyYebz <mannyyebz@gmail.com>
2026-08-14 23:58:14 -0400

    improved the loader.py

--- src/rag_project/loader.py
+++ src/rag_project/loader.py
@@ -1,16 +1,20 @@
-# from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-# loader = PyPDFLoader("data/attention.pdf")
-# documents = loader.load()
+loader = PyPDFLoader("data/attention.pdf")
+documents = loader.load()
 
-# # for doc in documents:
-# #     print("there were " +int((len(doc.page_content))) + "documents loaded")
-# print(documents)
 
-from pypdf import PdfReader
+for i,doc in enumerate(documents, start=1):
+    print(f" Page {i} has {len(doc.page_content)} characters")
+print(f'The document has {len(documents)} pages')
+#print(documents)
 
-reader = PdfReader("data/attention.pdf")
-for page in reader.pages:
-    print(page.extract_text()[:500])
-    print("---")
+# from pypdf import PdfReader
+
+# reader = PdfReader("data/attention.pdf")
+# for page in reader.pages:
+#     print(page.extract_text()[:500])
+#     print("---")
 
